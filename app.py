f31aae40632c659ab54564d7a911edcc28da015a
mikkildlebl <47360155+mikkildlebl@users.noreply.github.com>
2022-11-30 13:13:49 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request 
 
-app = Flask(__name__)
+app = Flask(__name__) 
+     
+@app.route('/')
+def index(): 
+    return render_template('index.html') 
+     
+@app.route('/', methods = ['POST']) 
+def get_data(): 
+    score = request.form['score']
+    
+    if request.method == 'POST': 
+        with open('data.txt', 'w') as f:
+            f.write("\n" + str(score))
+            f.close()
+    return render_template("index.html") 
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+     
+if __name__ == '__main__': 
+    app.run(debug = True)
